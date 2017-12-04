ece0345425c5633c7fb502ffa780496b6eabb5a4
pdbq21 <pdbq2122@gmail.com>
2017-12-04 19:47:50 +0200

    done: added new todo

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
+  <div>
     <input
       type="text"
       v-model="input_val"
       v-on:keydown="submit"
       placeholder="TODO"
     />
+  </div>
+  <div>
+    <ul>
+      <li v-for="todo in todos">
+        {{todo.value}}
+      </li>
+    </ul>
+  </div>
   </div>
 </template>
 
@@ -15,13 +24,21 @@
     data() {
       return {
         input_val: '',
+        todos: [],
       };
     },
     methods: {
       submit({ keyCode }) {
         if (keyCode === 13) {
-          console.log(this.input_val);
+          this.addTodo();
+          this.input_val = '';
         }
+      },
+      addTodo() {
+        this.todos.push({
+          value: this.input_val,
+          complete: false,
+        });
       },
     },
   };
