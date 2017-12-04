e474412d0c95812d14bea760d50da0b57becf5ef
pdbq21 <pdbq2122@gmail.com>
2017-12-04 19:38:25 +0200

    done: reading from input

---
+++ src/components/TodoList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input
+      type="text"
+      v-model="input_val"
+      v-on:keydown="submit"
+      placeholder="TODO"
+    />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'todo-list',
+    data() {
+      return {
+        input_val: '',
+      };
+    },
+    methods: {
+      submit({ keyCode }) {
+        if (keyCode === 13) {
+          console.log(this.input_val);
+        }
+      },
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
