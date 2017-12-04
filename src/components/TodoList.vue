c02dc5df72b302919c504c0ff463e26ccfdd75c5
pdbq21 <pdbq2122@gmail.com>
2017-12-04 20:23:52 +0200

    done: style

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,20 +1,19 @@
 <template>
-  <div>
-    <div>
+  <div class="container">
+    <div class="container_form">
       <input
+        class="container_form-input"
         type="text"
         v-model="input_val"
         v-on:keydown="submit"
         placeholder="TODO"
       />
     </div>
-    <div>
-      <ul>
-        <li v-for="(todo, index) in todos">
-          {{todo.value}}
-          <button v-on:click="deleteTodo(index)">X</button>
-        </li>
-      </ul>
+    <div class="container_todo_list">
+      <div class="container_todo_list-item" v-for="(todo, index) in todos">
+        <span>{{todo.value}}</span>
+        <button v-on:click="deleteTodo(index)">X</button>
+      </div>
     </div>
   </div>
 </template>
@@ -29,7 +28,7 @@
       };
     },
     methods: {
-      submit({keyCode}) {
+      submit({ keyCode }) {
         if (keyCode === 13) {
           this.addTodo();
           this.input_val = '';
@@ -49,5 +48,39 @@
 </script>
 
 <style scoped>
+  .container {
+    margin: 0 auto;
+    max-width: 25em;
+    min-width: 25em;
+  }
+
+  .container_form{
+    display: flex;
+  }
+  .container_form-input {
+    height: 2em;
+    flex-grow: 1;
+    padding: 1px 10px;
+    font-size: 25px;
+  }
+
+  .container_form-input:hover,
+  .container_form-input:focus {
+    outline: none;
+  }
+
+  .container_todo_list {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .container_todo_list-item {
+    font-size: 24px;
+    display: flex;
+    justify-content: space-between;
+    height: 2em;
+    align-items: center;
+    padding: 5px 10px;
+  }
 
 </style>
