e01821425263a8b6715e314797df2633d8558a77
pdbq21 <pdbq2122@gmail.com>
2017-12-07 20:38:37 +0200

    done: without route

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,28 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <todo-list/>
   </div>
 </template>
 
-
 <script>
-export default {
-  name: 'app',
-};
+  import TodoList from './components/TodoList';
+
+  console.log(TodoList);
+  export default {
+    components: { TodoList },
+    name: 'app',
+    component: { TodoList },
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
